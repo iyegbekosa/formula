52ec1b0ca79bb1e1cdf97f01589b174acc57db08
iyegbekosa <aikpitanyi.iyegbekosa@gmail.com>
2022-10-26 15:17:10 +0100

    lol

---
+++ dr_ajayi/eq1.py
@@ -0,0 +1,13 @@
+def sum():
+    if type(num1) == type(num2) == 1:
+        print("calculatioon")
+    else :
+        print("cannot process")
+        
+num1 = int(input("num 1:"))
+num2 = int(input("num 2:"))
+
+
+sum(num1, num2)
+
+# print(type(int(input("num 1:"))))
