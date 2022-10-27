8cb7cc3dcf3d05c9de523edc4a8d78bd1734fdad
iyegbekosa <aikpitanyi.iyegbekosa@gmail.com>
2022-10-27 12:31:56 +0100

    body roundness index

--- dr_ajayi/squareroot.py
+++ dr_ajayi/squareroot.py
@@ -1,5 +1,33 @@
 import math
+# wc = 'Waist circumfrence'
+# x = 'PI'
+# h = 'height'
+# w = 'weight'
+# bmi = 'bmi'
+# hc = 'hip circumfrence'
+# s = 'waist'
+# l = 'hip'
 
-print(math.sqrt(16))
 
-print(2/3)
+# bri = -1.3000000000000114 * math.sqrt(1-((wc/(2*x))**2)/((0.5*h)**2)) #body roundness index
+
+class formula():
+    def body_ri():
+        wc = float(input("Waist circumfrence: "))
+        h = float(input("Height: "))
+        x = float(input("PI: "))
+        q = "string"
+        
+        if type(wc) == type(x) == type(h) == type(1.0):
+            bri = -1.3000000000000114 * math.sqrt(1-((wc/(2*x))**2)/((0.5*h)**2))
+            print(bri)
+        
+        elif type(wc) == type(q) or type(x) == type(q) or type(h) == type(q):
+            print("Value must not be string")
+            
+        else :
+            ('invalid input')
+            
+
+
+formula.body_ri()
